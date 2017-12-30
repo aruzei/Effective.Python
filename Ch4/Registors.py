59edc23579be79b6df514c69d7cfaeabf2adff25
aruzei <szk11735@gmail.com>
2017-12-30 16:06:43 +0900

    Complete item29.

---
+++ Ch4/Registors.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+CHAPTER3
+"""
+
+class Registor(object):
+    """
+    Registor
+    """
+    def __init__(self, ohms):
+        self.ohms = ohms
+        self.voltage = 0
+        self.current = 0
+
+
+class VolatageRegistance(Registor):
+    """
+    VolatageRegistance
+    """
+    def __init__(self, ohms):
+        super().__init__(ohms)
+        self._voltage = 0
+
+   
+    @property
+    def voltage(self):
+        return self._voltage
+
+
+    @voltage.setter
+    def voltage(self, voltage):
+        """
+        Set voltage and calculate currnet.
+        """
+        self._voltage = voltage
+        self.current = self._voltage / self.ohms
+
+class BoundedRegistance(Registor):
+    """
+    BoundedRegistance
+    """
+    def __init__(self, ohms):
+        super().__init__(ohms)
+        self._ohms = self.ohms
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        """
+        If ohms is equal or less than 0, ValueError occurs.
+        """
+        if ohms <= 0:
+            raise ValueError('%f ohms must be > 0' % ohms)
+        self._ohms = ohms
+
+class ImmutableRegistance(Registor):
+    """
+    ImmutableRegistance
+    """
+    def __init__(self, ohms):
+        super().__init__(ohms)
+        self._ohms = self.ohms
+
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        """
+        This property is read only. If use this, AttributeError occurs.
+        """
+        if hasattr(self, '_ohms'):
+            raise AttributeError("Can't be set attribute")
+        self._ohms = ohms
---
+++ Ch4/item29.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+CHAPTER3
+"""
+
+from Registors import VolatageRegistance
+from Registors import BoundedRegistance
+from Registors import ImmutableRegistance
+
+REGISTOR = VolatageRegistance(1e3)
+REGISTOR.voltage = 10
+print("%5r amps" % REGISTOR.current)
+
+BOUNDED = BoundedRegistance(10e3)
+try:
+    BOUNDED.ohms = 0
+except ValueError as ex:
+    print(ex)
+
+IMUUTABLE = ImmutableRegistance(5e2)
+try:
+    IMUUTABLE.ohms = 10
+except AttributeError as ex:
+    print(ex)
